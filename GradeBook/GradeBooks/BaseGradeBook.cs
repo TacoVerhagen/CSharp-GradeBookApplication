dc222e1a9153c13cf7f2b6810a61034edadac09b
Taco Verhagen <Taco.Verhagen@achmea.nl>
2020-03-02 17:13:30 +0100

    Poging 2, opgave 1

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,7 +14,7 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
 
-        public GradeBookType Type;
+        public GradeBookType Type { get; protected set; }
 
         public BaseGradeBook(string name)
         {
