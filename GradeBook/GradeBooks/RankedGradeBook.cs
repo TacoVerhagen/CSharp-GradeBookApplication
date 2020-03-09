c75b77382e24592678ee05a17b9ead2ffe9757c6
Taco Verhagen <Taco.Verhagen@achmea.nl>
2020-03-09 21:19:52 +0100

    Poging 1

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,5 @@
 ﻿using GradeBook.Enums;
 using System;
-using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -18,22 +17,43 @@ namespace GradeBook.GradeBooks
 
             int nrOfStudents = Students.Count;
             Students.Sort((s1, s2) => s2.AverageGrade.CompareTo(s1.AverageGrade));
-            if (Students[nrOfStudents/5-1].AverageGrade <= averageGrade)
+            if (Students[nrOfStudents / 5 - 1].AverageGrade <= averageGrade)
             {
                 return 'A';
-            } else if (Students[2 * nrOfStudents / 5-1].AverageGrade <= averageGrade)
+            }
+            else if (Students[2 * nrOfStudents / 5 - 1].AverageGrade <= averageGrade)
             {
                 return 'B';
             }
-            else if (Students[3 * nrOfStudents / 5-1].AverageGrade <= averageGrade)
+            else if (Students[3 * nrOfStudents / 5 - 1].AverageGrade <= averageGrade)
             {
                 return 'C';
             }
-            else if (Students[4 * nrOfStudents / 5-1].AverageGrade <= averageGrade)
+            else if (Students[4 * nrOfStudents / 5 - 1].AverageGrade <= averageGrade)
             {
                 return 'D';
             }
             return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
         }
     }
 }
