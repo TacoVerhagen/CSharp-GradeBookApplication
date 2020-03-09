618e9637f33b376908acf9f5311aa800291fd9e4
Taco Verhagen <Taco.Verhagen@achmea.nl>
2020-03-09 21:13:53 +0100

    Poging 1

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,7 +1,6 @@
 ﻿using GradeBook.Enums;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -10,6 +9,31 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+
+            int nrOfStudents = Students.Count;
+            Students.Sort((s1, s2) => s2.AverageGrade.CompareTo(s1.AverageGrade));
+            if (Students[nrOfStudents/5-1].AverageGrade <= averageGrade)
+            {
+                return 'A';
+            } else if (Students[2 * nrOfStudents / 5-1].AverageGrade <= averageGrade)
+            {
+                return 'B';
+            }
+            else if (Students[3 * nrOfStudents / 5-1].AverageGrade <= averageGrade)
+            {
+                return 'C';
+            }
+            else if (Students[4 * nrOfStudents / 5-1].AverageGrade <= averageGrade)
+            {
+                return 'D';
+            }
+            return 'F';
         }
     }
 }
